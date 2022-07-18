a41f2c67def46acbf57b3d6635c699b5e98992d2
puradak <bbln2252@naver.com>
2022-07-18 15:29:31 +0900

    refactor names

--- app/src/main/java/com/example/chatting/MainPage.java
+++ app/src/main/java/com/example/chatting/MainPage.java
@@ -3,7 +3,7 @@ package com.example.chatting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
