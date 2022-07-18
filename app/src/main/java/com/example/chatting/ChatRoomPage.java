804a8dae825a63a99a0bd0c9310667010c6d3a00
puradak <bbln2252@naver.com>
2022-07-18 15:32:50 +0900

    refactor names

---
+++ app/src/main/java/com/example/chatting/ChatRoomPage.java
@@ -0,0 +1,14 @@
+
+package com.example.chatting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatRoomPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room_page);
+    }
+}
