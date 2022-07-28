c9aed8a683ca253f34f40ba6b07fa14db96a9c8c
jung0115 <jungim7490@gmail.com>
2022-07-29 07:10:22 +0900

    save

--- app/src/main/java/com/example/chatting/ChatRoomPage.java
+++ app/src/main/java/com/example/chatting/ChatRoomPage.java
@@ -1,8 +1,12 @@
-
+// 채팅방 페이지
 package com.example.chatting;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.chatting.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatRoomPage extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ChatRoomPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room_page);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();        // 데이터베이스 선언, 할당
+        DatabaseReference myRef = database.getReference("HasoyeonChat").child("ChatMassages");
+
+        myRef.setValue("Test Message!");
     }
 }
